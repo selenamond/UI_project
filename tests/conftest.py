b211d407c5810e757876c74c98d52169a0dca34e
elenautkina <selenamondeu@gmail.com>
2023-10-09 13:11:34 +0300

    add selenoid to conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,37 @@
 import pytest
 from selene import browser
-from webdriver_manager.core import driver
+import os
+from selenium import webdriver
+from dotenv import load_dotenv
 from utils import attach
+from selenium.webdriver.chrome.options import Options
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
+
+
+@pytest.fixture(scope='function', autouse=True)
 def setup_browser():
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": '100.0',
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True,
+            "sessionTimeout": "30m"
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+    driver = webdriver.Remote(
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
     browser.config.driver = driver
     browser.config.window_width = 1920
     browser.config.window_height = 1080
@@ -17,4 +43,5 @@ def setup_browser():
     attach.add_screenshot(browser)
     attach.add_logs(browser)
     attach.add_video(browser)
-    browser.quit()
+
+    driver.quit()
